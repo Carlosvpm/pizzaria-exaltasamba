d235f55fb87662fb73a2bef8160dcea33f29eb69
Carlos Moraes <moraesvinicius_2002@hotmail.com>
2022-09-15 10:21:17 -0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     environment: "jsdom",
   },
   server: {
-    port: process.env.PORT || 3000,
+    port: Number(process.env.PORT) || 3000,
     host: "0.0.0.0",
   },
 });
