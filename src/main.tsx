f7588c4e906a92e8347d6b666d12764508c2abd0
Carlos Moraes <moraesvinicius_2002@hotmail.com>
2022-09-14 21:50:35 -0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
-import { Navbar } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Header } from './components/header/Header';
 import './index.css'
 import { Home } from './pages/home/Home';
 
